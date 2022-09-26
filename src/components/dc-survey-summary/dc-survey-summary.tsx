488e0145aaf401d840194d0da466bb1cb9397565
Andrew Turner <ajturner@highearthorbit.com>
2022-09-26 00:01:31 -0400

    fixed summary to note include footnotes

--- src/components/dc-survey-summary/dc-survey-summary.tsx
+++ src/components/dc-survey-summary/dc-survey-summary.tsx
@@ -15,15 +15,37 @@ export class DcSurveySummary {
   calculatePercentage(value: number): number {
     return Math.ceil(value / this.numberResponses * 100)
   }
+  formatResponse(text:string):string {
+    return text.replace(/^\d+\./,'').replace(/_(.*)_/, '').trim();
+  }
   renderEnumerationSummary(question) {
     // console.debug("dc-survey-summary: renderEnumeration", {question});
     const responses = shuffle(question.responses, question.question.Sort);
+
+    // Responses might have footnotes that should be left out of summary
+    const summary = responses.reduce((output, response) => {
+      const r = this.formatResponse( response.response );
+      if(!output[r]) {
+        output[r] = {
+          response: r,
+          count: 0
+        }
+      }
+      output[r].count += response.candidates.length;
+      return output;
+    }, {})
+    console.debug("dc-survey-summery: renderEnumerationSummary", summary);
     return (
       <dl>
-        <dt class="question"><dc-election-query question={question.question}></dc-election-query></dt>
-        {responses.map((response) => {
-          return (<dd class={`percentage percentage-${this.calculatePercentage(response.candidates.length)}`}>
-            <span class="text">{response.response.replace(/^\d+\./,'')}: {response.candidates.length}</span>
+        <dt class="question">
+          <dc-election-query question={question.question}></dc-election-query>
+        </dt>
+        {Object.keys(summary).map((response) => {
+          return (
+          <dd class={`percentage percentage-${this.calculatePercentage(summary[response].count)}`}>
+            <span class="text">
+              {response}: {summary[response].count}
+            </span>
           </dd>)
         })}
       </dl>
